4d2a4061a67e9ac5ca30908a1417d5382d2ce691
Talin Daga <talindaga692@gmail.com>
2026-04-22 02:12:16 +0530

    user visibility on admin panel

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Address
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display  = ['email', 'company_name', 'phone_number', 'gst_number', 'is_verified_b2b', 'is_staff', 'date_joined']
+    list_filter   = ['is_verified_b2b', 'is_staff', 'is_active']
+    search_fields = ['email', 'company_name', 'gst_number', 'phone_number']
+    ordering      = ['-date_joined']
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('B2B Details', {
+            'fields': ('company_name', 'gst_number', 'phone_number', 'is_verified_b2b')
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('B2B Details', {
+            'fields': ('email', 'company_name', 'gst_number', 'phone_number', 'is_verified_b2b')
+        }),
+    )
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display  = ['user', 'address_line_1', 'city', 'state', 'pincode', 'is_default']
+    list_filter   = ['state', 'is_default']
+    search_fields = ['user__email', 'city', 'pincode']
